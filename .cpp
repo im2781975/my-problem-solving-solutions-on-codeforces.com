a718282c5390c7c0125ef295e54ca3bf2ef6dee6
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-02-24 17:41:03 +0100

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,6 +1,6 @@
 using namespace std;
 // A. Theatre Square
-// problemset/problem/1/A?mobile=false
+// problemset/problem/1/A
 int main(){
     unsigned long long n, m, a; cin >> n >> m >> a;
     cout << ceil((double)m / a) * ceil((double)n / a);
