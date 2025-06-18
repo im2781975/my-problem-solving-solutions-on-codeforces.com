ffcda855259c291fcdc05df0ba5b5592cdb360c0
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-06-18 15:43:09 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -19,6 +19,31 @@ int main(){
             cout << "OK";
         mp[str]++;
     }
+}#include <iostream>
+#include <string>
+using namespace std;
+http://codeforces.com/contest/16/problem/A
+// A. Flag
+int main() {
+    int n, m; cin >> n >> m;
+    string flag[n];
+    for(int i = 0; i < n; ++i)
+        cin >> flag[i];
+    bool isValid = true;
+    for(int i = 0; i < n; ++i){
+        for(int j = 1; j < m; ++j){
+            if(flag[i][j] != flag[i][0]){
+                isValid = false;
+                break;
+            }
+        }
+        if(i > 0 && flag[i][0] == flag[i - 1][0]){
+            isValid = false;
+            break;
+        }
+    }
+    cout << (isValid ? "YES" : "NO") << endl;
+    return 0;
 }
 using namespace std;
 // A. IQ test
