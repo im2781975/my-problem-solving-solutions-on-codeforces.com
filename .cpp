c0cae2aa779a10f95a66f9c8ea6c6804a18d748b
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-07-11 15:58:11 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -966,6 +966,31 @@ int main(){
     cout << arr[0];
 }
 using namespace std;
+http://codeforces.com/contest/408/problem/B
+// B. Garland
+int arr[26], arr2[26];
+int main(){
+    string s, s2; cin >> s >> s2;
+    int sum = 0;
+    int len1 = s.length();
+    int len2 = s2.length();
+    for(int i = 0; i < len1; i++)
+        ++arr[s[i] - 'a'];
+    for(int i = 0; i < len2; i++)
+        ++arr2[s2[i] - 'a'];
+    for(int i = 0; i < 26; i++){
+        if(arr2[i]){
+            if(arr[i])
+                sum += min(arr[i], arr2[i]);
+            else{
+                sum = 0; break;
+            }
+        }
+    }
+    if(sum) cout << sum;
+    else cout << -1;
+}
+using namespace std;
 // A. Keyboard
 // contest/474/problem/A
 int main(){
