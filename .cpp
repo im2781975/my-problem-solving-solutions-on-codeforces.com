9461443c2f721688f2d8c4519490ed216b92db71
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-07-17 16:37:51 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -1411,6 +1411,56 @@ int main() {
     cout << result << endl;
 }
 using namespace std;
+http://codeforces.com/contest/567/problem/A
+// 567A - Lineland Mail
+int main() {
+    int n; cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
+    for (int i = 0; i < n; ++i) {
+        int minDist, maxDist;
+        if (i == 0){
+            minDist = abs(arr[i] - arr[i + 1]);
+            maxDist = abs(arr[i] - arr[n - 1]);
+        } else if (i == n - 1) {
+            minDist = abs(arr[i] - arr[i - 1]);
+            maxDist = abs(arr[i] - arr[0]);
+        } else {
+            minDist = min(abs(arr[i] - arr[i - 1]), abs(arr[i] - arr[i + 1]));
+            maxDist = max(abs(arr[i] - arr[0]), abs(arr[i] - arr[n - 1]));
+        }
+        cout << minDist << " " << maxDist << "\n";
+    }
+}
+using namespace std;
+int main() {
+	 int n; cin >> n;
+	 int arr[n];
+	 for(int i=0;i<n;i++){
+		 cin>>arr[i];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+	for(int i = 0; i < n; i++){
+		long long  mini, maxi;
+		if(!i){
+			mini=abs(arr[i]-arr[i+1]);
+			maxi=abs(arr[i]-arr[n-1]);
+		}
+		else if(i == n-1) {
+			 maxi = abs(arr[i] - arr[0]);
+			 mini = abs(arr[i] - arr[i - 1]);
+		}
+		else{
+			 mini = min(abs(arr[i] - arr[i + 1]), abs(arr[i] - arr[i - 1]));
+			 maxi = max(abs(arr[i] - arr[0]), abs(arr[i] - arr[n - 1]));
+		}
+		 cout<<mini<<" "<<maxi<<"\n";
+	}
+	return 0;
+}
+using namespace std;
 // problemset/problem/617/A
 // A. Elephant
 int main(){
