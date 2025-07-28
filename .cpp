52b1055e87a80c8d971829d1ea60fcba2d508868
ibrahim_molla <140054583+im2781975@users.noreply.github.com>
2025-07-28 19:30:01 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -2253,6 +2253,22 @@ int main() {
     return 0;
 }
 using namespace std;
+https://codeforces.com/problemset/problem/518/A
+// A. Vitaly and Strings
+int main() {
+    string s, s2; cin >> s >> s2;
+    int len = s.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (s[i] == 'z') s[i] = 'a';
+        else {
+            s[i]++;
+            break;
+        }
+    }
+    if (s < s2) cout << s;
+    else cout << "No such string";
+}
+using namespace std;
 http://codeforces.com/problemset/problem/535/B
 // B.Tavas_and_SaDDas
 vector<int> lucky_numbers;
